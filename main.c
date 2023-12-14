b2e00e1052b2515bccae8d0c6687b83d7359aef3
Shubham Varne <89720812+ShubhamVarne@users.noreply.github.com>
2023-12-14 21:51:24 +0530

    Register Usage comments added

--- main.c
+++ main.c
@@ -7,21 +7,22 @@
 #define r32(addr) *((volatile unsigned int *) addr)
 
 void uart_init (void) {
-    w32(0xFE215004, 1);
-    w32(0xFE215044, 0);
-    w32(0xFE215060, 0);
-    w32(0xFE21504c, 3);
-    w32(0xFE215050, 0);
-    w32(0xFE215044, 0);
-    w32(0xFE215048, 0xC6);
-    w32(0xFE215068, ((200000000/(115200*8))-1));
-    w32(0xFE200004, ((2 << 12) | (2 << 15)));
-    w32(0xFE215060, 3);
+    w32(0xFE215004, 1);                             //Auxiliary enables
+    w32(0xFE215044, 0);                             //Mini UART Interrupt Enable
+    w32(0xFE215060, 0);                             //Mini UART Extra Control
+    w32(0xFE21504c, 3);                             //Mini UART Line Control
+    w32(0xFE215050, 0);                             //Mini UART Modem Control
+    w32(0xFE215044, 0);                             //Mini UART Interrupt Enable
+    w32(0xFE215048, 0xC6);                          //Mini UART Interrupt Identify
+    w32(0xFE215068, ((200000000/(115200*8))-1));    //Mini UART Baudrate
+    w32(0xFE200004, ((2 << 12) | (2 << 15)));       //GPIO Function Select 1
+    w32(0xFE215060, 3);                             //Mini UART Extra Control
+
 }
 
 void uart_putc (const char c) {
-    while (!(r32(0xFE215054) & 0x20));
-    w32(0xFE215040, (unsigned int) c);
+    while (!(r32(0xFE215054) & 0x20));              //Mini UART Line Status
+    w32(0xFE215040, (unsigned int) c);              //Mini UART I/O Data
 }
 
 void uart_puts(const char* s) {
